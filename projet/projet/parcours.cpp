95b58352830a532922dfc04ed9c1b0596b61008f
Martin Pouliot <martinpouliot@service-WIFI-EDUROAM-LA-9-142.nat.polymtl.ca>
2018-04-05 15:32:38 -0400

    Avant refactoring

--- projet/projet/parcours.cpp
+++ projet/projet/parcours.cpp
@@ -1,5 +1,5 @@
 #include "parcours.h"
-#define defaultSpeed 50
+#define defaultSpeed 40
 
 enum State {WALL_0, WALL_1, NONE, READY};
 
@@ -10,17 +10,28 @@ void Parcours::exec(){
 	State state = NONE;
 	bool isRunning = true;
 	uint8_t cntReady = 0;
-	uint8_t adjustementCnt = 0;
-	bool inAdjustement = true;
 	light::green();
+	uint8_t lastValue0 = 0;
+	uint8_t lastValue1 = 0;
+	uint8_t currentValue0 = 0;
+	uint8_t currentValue1 = 0;
 	while(isRunning){
+		currentValue0 = sensor.read0();
+		currentValue1 = sensor.read1();
+		uart::print("0_");
+		uart::print(currentValue0);
+		uart::print(" ");
+		uart::print("1_");
+		uart::print(currentValue1);
+		uart::println();
+
 		switch(state){
 			case NONE:
 				cntReady++;
 				if(cntReady >= 60)
 					state = READY;
 			case READY:
-				if(sensor.read0() > sensor.read1()){
+				if(currentValue0 > currentValue1){
 					state = WALL_1;
 				}else{
 					state = WALL_0;
@@ -30,42 +41,40 @@ void Parcours::exec(){
 			break;
 
 			case WALL_0:
-				//Adjustement for the wall
-
-				if(sensor.read0() > 20){
+				if(currentValue0 > 16){
 					light::red();
+					pwm::setA(60);
 
-					inAdjustement = true;
-					if(adjustementCnt < 50){
-						pwm::setA(75);
+					if(currentValue0 > lastValue0){
+						pwm::setB(-40);
 					}else{
-						pwm::setA(defaultSpeed);
 						pwm::setB(defaultSpeed);
 					}
-
 				}
-				else if(sensor.read0() < 15){
+				else if(currentValue0 < 13){
+					
 					light::red();
-					pwm::setB(75);
-					inAdjustement = true;
+					pwm::setB(60);
+					
+					//Mur à 90 le virage doit etre plus seree
+					if(currentValue0 < lastValue0){
+						pwm::setA(-40);
+					}else{
+						pwm::setA(defaultSpeed);
+					}
 				}
 				else{
-					inAdjustement = false;
 					light::green();
 					pwm::setA(defaultSpeed);
 					pwm::setB(defaultSpeed);
-				}
-				if(inAdjustement){
-					if(adjustementCnt < 50)
-					adjustementCnt++;
-				}else{
-					adjustementCnt = 0;
 				}
 
 			break;
 			case WALL_1:
 			break;
 		}
-		_delay_ms(10);
+		lastValue0 = currentValue0;
+		lastValue1 = currentValue1;
+		_delay_ms(50);
 	}
 }
