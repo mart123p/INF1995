4d532983b979b690b069977c39d786571a2bb418
Martin Pouliot <martinpouliot@service-WIFI-EDUROAM-LA-9-142.nat.polymtl.ca>
2018-04-06 08:51:41 -0400

    Up to date

--- projet/projet/parcours.cpp
+++ projet/projet/parcours.cpp
@@ -1,7 +1,7 @@
 #include "parcours.h"
 #define defaultSpeed 40
 
-enum State {WALL_0, WALL_1, READY};
+enum State {WALL_0,BIG_TURN_0,  WALL_1,BIG_TURN_1, READY};
 
 Parcours::Parcours(){
 	lastValue0 = 0;
@@ -38,19 +38,28 @@ void Parcours::exec(){
 
 			case WALL_0:
 				if(currentValue0 > 60){
-					//Trou
-					Parcours::virage90_0();
+					state = BIG_TURN_0;
 				}else{
 					ajustement0();
 				}
 			break;
+			case BIG_TURN_0:
+				Parcours::virage90_0();
+				state = WALL_0;
+			break;
+
+	
 			case WALL_1:
 				if(currentValue1 > 60){
-					//Trou
-					Parcours::virage90_1();
+					state = BIG_TURN_1;
 				}
 				ajustement1();
 			break;
+			case BIG_TURN_1:
+				Parcours::virage90_1();
+				state = WALL_1;
+			break;
+
 		}
 		lastValue0 = currentValue0;
 		lastValue1 = currentValue1;
