e4f216dc1c04cc9784d8a79165130674a2cdce92
Martin Pouliot <contact@martinpouliot.ca>
2018-04-13 18:46:51 -0400

    Correction .h pour enlever le warning, et ignorer certains etats dans poteau.cpp

--- projet/projet/poteau.cpp
+++ projet/projet/poteau.cpp
@@ -61,6 +61,13 @@ void Poteau::scrutation(Sensor& sensor,State& state, State& lastState){
 	//On fait le code 2 fois, une fois pour chaque cote des capteurs.
 	if(state != READY && tick > 25){
 		for(uint8_t i = 0; i < 2; i++){
+
+			if(i == 0 && (state == WALL_0 || state == BIG_TURN_0 || state == GROS_AJUSTEMENT_0)){
+				break; // On ignore la scrutation du capteur 0 s'il est dans les états précédents
+			}
+			if(i == 1 && (state == WALL_1 || state == BIG_TURN_1 || state == GROS_AJUSTEMENT_1)){
+				break; // On ignore la scrutation du capteur 1 s'il est dans les états précédents
+			}
 			uint8_t* oldVals;
 			
 			//Choisis si on est sur le cote 0 ou 1 du robot.
